857f64736ddd35e07b202d0ef5ee66481a9814ae
dario.karmeinski <dario.karmeinski@gmail.com>
2023-01-03 20:32:14 +0100

    Adds logging

---
+++ main/settings/dev.py
@@ -0,0 +1,36 @@
+from .base import INSTALLED_APPS, MIDDLEWARE, BASE_DIR
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+THIRD_PARTY_APPS = [
+    'debug_toolbar',
+]
+
+INSTALLED_APPS += THIRD_PARTY_APPS
+
+
+THIRD_PARTY_MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+MIDDLEWARE += THIRD_PARTY_MIDDLEWARE
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
